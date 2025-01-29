1851ae6b61737c59f0d94fadc256ec10b4fa0f1e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-29 12:18:09 +0000

    Updating to latest FlutterFlow output.

--- lib/components/limit_views_model.dart
+++ lib/components/limit_views_model.dart
@@ -1,15 +1,15 @@
 import '/backend/supabase/supabase.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import 'empty_chats_widget.dart' show EmptyChatsWidget;
+import 'limit_views_widget.dart' show LimitViewsWidget;
 import 'package:flutter/material.dart';
 
-class EmptyChatsModel extends FlutterFlowModel<EmptyChatsWidget> {
+class LimitViewsModel extends FlutterFlowModel<LimitViewsWidget> {
   ///  State fields for stateful widgets in this component.
 
   // Stores action output result for [Backend Call - Query Rows] action in Button widget.
-  List<UsersRow>? userd;
+  List<UsersRow>? user;
   // Stores action output result for [Backend Call - Query Rows] action in Button widget.
-  List<MatchTrackingRow>? matchTrack;
+  List<EmpresasRow>? empresa;
 
   @override
   void initState(BuildContext context) {}
